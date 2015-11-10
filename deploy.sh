2a1e1f0064246fb2bca3b7af4dc9bf9e2a72fd12
Richard Gill <richard@rgill.co.uk>
2015-11-10 11:52:52 +0000

    Corrected echo's

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 echo "Deploying your software!"
 
-echo "Shipit : $(SHIPIT)"
-echo "SHIPIT_LINK : $(SHIPIT_LINK)"
-echo "USER: $(USER)"
-echo "EMAIL: $(EMAIL)"
-echo "ENVIRONMENT: $(ENVIRONMENT)"
-echo "LAST_DEPLOYED_SHA: $(LAST_DEPLOYED_SHA)"
+echo "Shipit : $SHIPIT"
+echo "SHIPIT_LINK : $SHIPIT_LINK"
+echo "USER: $USER"
+echo "EMAIL: $EMAIL"
+echo "ENVIRONMENT: $ENVIRONMENT"
+echo "LAST_DEPLOYED_SHA: $LAST_DEPLOYED_SHA"
 
 echo "printenv $(printenv)"
